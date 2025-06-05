9225b0a035a2df5fb49431f9b523cf04c0508e65
sundayknight98 <112872923+sundayknight98@users.noreply.github.com>
2025-06-05 23:41:55 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 66, 15, 153),
+                const Color.fromARGB(255, 86, 50, 148),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
