4ecae38e20e4652a01d76b6731a619949bb6ae7b
sundayknight98 <112872923+sundayknight98@users.noreply.github.com>
2025-06-06 16:06:22 +0800

    Add files via upload

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,7 +9,18 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300),
+          // ),
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
